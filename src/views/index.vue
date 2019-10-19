e7b667fdf8766f438795a31cee30a2b743a9f9cd
HuAo1105 <2373685495@qq.com>
2019-10-19 15:39:02 +0800

    22

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    你可长得真好看呀！
+    <!-- <div class="mt">
+      你可长得真好看呀！
+    </div> -->
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
